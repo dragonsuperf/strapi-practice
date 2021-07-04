3f56082eb79c38e7e8802d022283ea2ccfe4b779
dragonsuperf <dragonsuperf@gmail.com>
2021-07-04 15:52:28 +0900

    init

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b562bfa6898af7bb2e27bd4e4652d228'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b609a4a4-d313-49ff-a43d-7c90546e18aa'
+};
